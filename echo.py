d5fd77d0928b8fe8e739a113fc439e5205474b88
Diver - home(win) <sysdeep@yandex.ru>
2017-03-29 20:12:51 +0300

    start

---
+++ echo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import telebot
+
+BOT_TOKEN = "349973004:AAEZYylLlwfcIyLwxkaE-veGWjRB9YPBcVM"
+
+
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): 									# Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
+
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
